878e359629b459a68618905fad11b27b156b9f03
naomichi masumoto <naomichimasumoto@michi-2022MacBook-Pro.local>
2023-10-23 13:01:54 +0900

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,3 +5,26 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
 })
+
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+// import vueImages from 'vite-plugin-vue-images'
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [
+//     react(),
+//     vueImages()
+//   ],
+//   build: {
+//     rollupOptions: {
+//       output: {
+//         manualChunks: undefined,
+//       },
+//     },
+//   },
+//   esbuild: {
+//     jsxFactory: 'h',
+//     jsxFragment: 'Fragment',
+//   },
+// });
