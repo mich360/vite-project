33dbda4302ca9fa6462bcbd392a6f6bbb4e07576
naomichi masumoto <naomichimasumoto@michi-2022MacBook-Pro.local>
2023-10-23 13:01:49 +0900

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,23 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+// import queenImage from './images/queen.png'; // 画像のインポート
+
+// // 
+
+// import ReactDOM from 'react-dom';
+
+
+
+
+
+// ReactDOM.render(imageElement, document.getElementById('image-container'));
+
+// // 画像を表示する
+// const imageElement = <img src={queenImage} alt="Queen Image" />;
+// render(imageElement, document.getElementById('image-container'));
+
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
