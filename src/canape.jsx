d039c991d57c4bdf9c1f359495291b39a7788e41
naomichi masumoto <naomichimasumoto@michi-2022MacBook-Pro.local>
2023-10-25 18:29:43 +0900

    Create canape.jsx

---
+++ src/canape.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// コンポーネントの定義
+function MyComponent() {
+  return <h1>Hello, React!</h1>;
+}
+
+// コンポーネントをルート要素にレンダリング
+ReactDOM.render(<MyComponent />, document.getElementById('root'));
+
+export default Canape;
