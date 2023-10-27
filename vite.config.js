d54a92f1100c0fb0f58c814149074604419e4a92
naomichi masumoto <naomichimasumoto@michi-2022MacBook-Pro.local>
2023-10-27 11:50:46 +0900

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,16 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './', // ルートパスを設定
 })
+
+// vite.config.js
+
+
+
+
+
+
 
 // import { defineConfig } from 'vite'
 // import react from '@vitejs/plugin-react'
