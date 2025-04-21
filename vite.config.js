08a54a970adaacd1a060eb809a91f15d2f243f68
mich360 <fghbg751@yahoo.co.jp>
2025-04-21 17:39:14 +0900

    Fix: include index.html in Vite build

--- vite.config.js
+++ vite.config.js
@@ -1,38 +1,12 @@
 import { defineConfig } from 'vite';
+import path from 'path';
 
 export default defineConfig({
   build: {
     rollupOptions: {
       input: {
-        main: './src/main.jsx', // main.jsx をエントリーポイントとして指定
+        main: path.resolve(__dirname, 'index.html'), // index.html をエントリーポイントとして追加
       },
     },
   },
 });
-
-
-
-
-
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-// import vueImages from 'vite-plugin-vue-images'
-
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [
-//     react(),
-//     vueImages()
-//   ],
-//   build: {
-//     rollupOptions: {
-//       output: {
-//         manualChunks: undefined,
-//       },
-//     },
-//   },
-//   esbuild: {
-//     jsxFactory: 'h',
-//     jsxFragment: 'Fragment',
-//   },
-// });
